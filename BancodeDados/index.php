78dda5c94ad123c89e9b664989e3cc5646392db3
DarlynNS <darlyn0907@gmail.com>
2024-11-15 17:03:09 -0300

    update 15/11

--- BancodeDados/index.php
+++ BancodeDados/index.php
@@ -1,6 +1,6 @@
 <?php
 
-include './model/Conexao.php';
+/*include './model/Conexao.php';
 include './model/lista.php';
 include './model/produto.php';
 include './model/usuario.php';
@@ -11,7 +11,7 @@ $c = new Controller();
 $c->index();
 
 if($c->cadastroIndex) echo $c->cadastroIndex;
-if($c->loginIndex) echo $c->loginIndex;
+if($c->loginIndex) echo $c->loginIndex;*/
 
 ?>
 
@@ -24,14 +24,35 @@ if($c->loginIndex) echo $c->loginIndex;
   <body>
         <section class="col-md-6">
             <form method="POST">
-                <hr>
                 <h2>
                     <strong> Cadastro de Cliente</strong>
                 </h2>
                 <hr>
                 <p><input type="text" name="nome" placeholder="Digite seu nome" required/></p>
 
+                <h2>
+                    <strong> Cadastro de Automovel</strong>
+                </h2>
+                <hr>
+                <p><input type="text" name="modelo" placeholder="Modelo: " required/></p>
+                <p><input type="number" name="preco" placeholder="Preço: " required/></p>
                 
+                <h2>
+                    <strong> Cadastro de Locação</strong>
+                </h2>
+                <hr>
+                <p><input type="number" name="area" placeholder="Area: " required/></p>
+                <p><input type="text" name="automovel" placeholder="Automovel: " required/></p>
+                <p><input type="text" name="concessio" placeholder="Concessionaria: " required/></p>
+                <p><input type="number" name="quantidade" placeholder="Quantidade: " required/></p>
+
+                <h2>
+                    <strong>Cadastro de Concessionaria</strong>
+                </h2>
+                <hr>
+                <p><input type="text" name="conce" placeholder="Concessionaria: " required/></p>
+                <hr>
+
                 <p><button name="cadastrar" type="submit">Cadastrar</button></p>
              </form>
         </section>
